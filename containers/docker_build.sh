db8cdfd34a8c4508ff996badcf07d24757c1b336
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-05-27 10:42:20 +0200

    fix tag format: <org>/<repo>:<tag>

--- containers/docker_build.sh
+++ containers/docker_build.sh
@@ -2,15 +2,17 @@
 set -xe
 
 ORG="easybuilders"
+REPO="base"
+OS="centos7"
 
 IMG_TEMP_TAG="temp-$$-${RANDOM}"
 if [[ -z ${EB_VER} ]];then
-	docker build -t ${ORG}:${IMG_TEMP_TAG} .
+	docker build -f Dockerfile.${REPO}-${OS} -t ${ORG}:${IMG_TEMP_TAG} .
 else
-	docker build --build-arg=EB_VER=${EB_VER} -t ${ORG}:${IMG_TEMP_TAG} .
+	docker build -f Dockerfile.${REPO}-${OS} --build-arg=EB_VER=${EB_VER} -t ${ORG}:${IMG_TEMP_TAG} .
 fi
 
 # determine final tag using EasyBuild version used in container
-IMG_TAG=base-centos7-eb$(docker inspect -f '{{.Config.Labels.easybuild_version}}' ${ORG}:${IMG_TEMP_TAG})
+IMG_TAG=${REPO}-${OS}-eb$(docker inspect -f '{{.Config.Labels.easybuild_version}}' ${ORG}:${IMG_TEMP_TAG})
 docker tag ${ORG}:${IMG_TEMP_TAG} ${ORG}:${IMG_TAG}
 docker rmi ${ORG}:${IMG_TEMP_TAG}
