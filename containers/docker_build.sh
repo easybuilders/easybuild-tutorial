67b308a8dc4b57b9118964bbc1e18b118182251e
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-06-04 10:54:19 +0200

    update docker_build.sh script

--- containers/docker_build.sh
+++ containers/docker_build.sh
@@ -2,17 +2,22 @@
 set -xe
 
 ORG="easybuilders"
-REPO="base"
+REPO="test"
 OS="centos7"
+EB="eb4.2.1"
+CPU_ARCH="haswell"
 
-IMG_TEMP_TAG="temp-$$-${RANDOM}"
-if [[ -z ${EB_VER} ]];then
-	docker build -f Dockerfile.${REPO}-${OS} -t ${ORG}/${REPO}:${IMG_TEMP_TAG} .
-else
-	docker build -f Dockerfile.${REPO}-${OS} --build-arg=EB_VER=${EB_VER} -t ${ORG}/${REPO}:${IMG_TEMP_TAG} .
-fi
+GCC="GCC-9.3.0"
+FOSS="foss-2020a"
+SCIPY_BUNDLE="SciPy-bundle-foss-2020a-Python-3.8.2"
 
-# determine final tag using EasyBuild version used in container
-IMG_TAG=${OS}-eb$(docker inspect -f '{{.Config.Labels.easybuild_version}}' ${ORG}/${REPO}:${IMG_TEMP_TAG})
-docker tag ${ORG}/${REPO}:${IMG_TEMP_TAG} ${ORG}/${REPO}:${IMG_TAG}
-docker rmi ${ORG}/${REPO}:${IMG_TEMP_TAG}
+COMMON_TAG=${ORG}/${REPO}:${OS}
+
+# build base container
+docker build -f Dockerfile.base-${OS} -t ${COMMON_TAG}-${EB}
+# build container for GCC-9.3.0 on Haswell
+docker build -f Dockerfile.easybuild-${OS} --build-arg CPU_ARCH=${CPU_ARCH} --build-arg EASYCONFIG=${GCC}.eb -t ${COMMON_TAG}-${CPU_ARCH}-${GCC} .
+# build container for foss-2020a on Haswell, on top of GCC-9.3.0 container
+docker build -f Dockerfile.easybuild-${OS} --build-arg CPU_ARCH=${CPU_ARCH} --build-arg IMG_NAME=${REPO} --build-arg IMG_TAG=${OS}-${CPU_ARCH}-${GCC} --build-arg EASYCONFIG=${FOSS}.eb -t ${ORG}/${REPO}:${OS}-${CPU_ARCH}-${FOSS} .
+# build software container with SciPy-bundle for Haswell, on top of foss-2020a container
+docker build -f Dockerfile.easybuild-${OS} --build-arg CPU_ARCH=${CPU_ARCH} --build-arg IMG_NAME=${REPO} --build-arg IMG_TAG=${OS}-${CPU_ARCH}-${FOSS} --build-arg EASYCONFIG=${SCIPY_BUNDLE}.eb -t ${ORG}/${REPO}:${OS}-${CPU_ARCH}-${SCIPY_BUNDLE} .
